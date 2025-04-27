2c549a9a3aee278f65499e78ad0753835170b4fc
Ynot400 <147223483+Ynot400@users.noreply.github.com>
2025-04-26 22:56:33 -0400

    removes the header at the top per request of daniel

--- AutoScheduler/app/_layout.tsx
+++ AutoScheduler/app/_layout.tsx
@@ -3,7 +3,10 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{
+      headerShown: false,
+    }}
+    >
       <Stack.Screen name="signin" options={{ title: "Sign In" }} />
       <Stack.Screen name="register" options={{ title: "Register" }} />
       <Stack.Screen name="home" options={{ title: "Home" }} />
