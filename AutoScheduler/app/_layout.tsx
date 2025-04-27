45e1a1b4af51e98a49ba3f2be838e9dbe8444571
Daniel L <87623782+MaxOltimo@users.noreply.github.com>
2025-04-26 22:53:29 -0400

    basic settings implementation

--- AutoScheduler/app/_layout.tsx
+++ AutoScheduler/app/_layout.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Stack } from "expo-router";
+import { ThemeProvider } from "../components/ThemeContext";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="signin" options={{ title: "Sign In" }} />
-      <Stack.Screen name="register" options={{ title: "Register" }} />
-      <Stack.Screen name="home" options={{ title: "Home" }} />
-      <Stack.Screen name="calendar" options={{ title: "Calendar" }} />  
-    </Stack>
-
+    <ThemeProvider> {/* Wrap everything inside ThemeProvider */}
+      <Stack>
+        <Stack.Screen name="signin" options={{ title: "Sign In" }} />
+        <Stack.Screen name="register" options={{ title: "Register" }} />
+        <Stack.Screen name="home" options={{ title: "Home" }} />
+        <Stack.Screen name="calendar" options={{ title: "Calendar" }} />
+      </Stack>
+    </ThemeProvider>
   );
 }
